469e1f0671fd7fd1349cf5682ed2f1745c7fff00
Kayque <kayquecarvalho020@gmail.com>
2026-08-03 11:25:20 -0300

    feat(c#-funamento): primeira ativiade, faz atividade de input para o usuário
    chore(gitignore): configura gitignore para o projeto

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Console.WriteLine("Hello, World!");
+
+// Aula 1: C# Software Developmente
+class Program {
+    static void Main(string[] args){
+        //Variáveis locais
+        string nome;
+        string curso;
+        int idade;
+
+        //Dados usuáio
+        Console.WriteLine("Salve");
+        Console.WriteLine("Qual o seu nome?");
+
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Bem vindo " + nome);
+        Console.WriteLine("Qual a sua idade? ");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Qual o seu curso? ");
+        curso = Console.ReadLine();
+
+        //Printa dados de usuário no console 
+        Console.WriteLine("===Dados Usuário===");
+        Console.WriteLine($"Nome: {nome} \nIdade: {idade} \nCurso: {curso}");
+    }
+}
---
+++ anotacoes.md
@@ -0,0 +1,10 @@
+# C# DEVELOPMENT
+
+03/08/2026
+
+Linguagem criada pela Microsoft em 2000
+Ela é uma linguagem compilada e fortimente tipada(menos mutável), com ``garbage collector`` (aclopa memória e desaclopa, limpando a memória automáticamente para evitar travamento)
+
+
+C# Dev kit para VS code
+Recomendado: SK 8.0 (LTS) ou superior (**sempre usar LTS**)
